e74487fa5ef65595b46e7b488fb4adabd5c6d557
PearlSavvy <161421954+PearlSavvy@users.noreply.github.com>
2024-07-23 19:37:30 +0200

    adding basic Api

--- src/index.js
+++ src/index.js
@@ -1,8 +1,13 @@
-function handleSearchSubmit (event){
-    event.preventDefault();
-    let searchInput= document.querySelector("#search-form-input");
-    let cityElement= document.querySelector("#city")
-    cityElement.innerHTML= searchInput.value;
+function searchCity(city) {
+  let apiKey = "b2a5adcct04b33178913oc335f405433";
+  let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}`;
+  axios.get(apiUrl).then(refreshWeather);
+
+function handleSearchSubmit(event) {
+  event.preventDefault();
+  let searchInput = document.querySelector("#search-form-input");
+  let cityElement = document.querySelector("#city");
+  cityElement.innerHTML = searchInput.value;
 }
 
 let searchFormatElement = document.querySelector("search-form");
